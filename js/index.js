230800313d7150cdbb771935f0964577028f8138
Mehedimid <mehedihasan25276@gmail.com>
2023-08-29 23:44:37 +0600

    added modal

--- js/index.js
+++ js/index.js
@@ -1,30 +1,30 @@
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText, isShow) => {
   const response = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${searchText}`
   );
   const data = await response.json();
   const phones = data.data;
-  displayPhone(phones);
+  displayPhone(phones, isShow);
 };
-const displayPhone = (phones) => {
-    // get card container div
-    const cardContainer = document.getElementById("card-container")
+const displayPhone = (phones, isShow) => {
+  // get card container div
+  const cardContainer = document.getElementById("card-container");
 
-    // clear fld after search any phone 
-    cardContainer.textContent = ''
+  // clear fld after search any phone
+  cardContainer.textContent = "";
 
-    // condition 
-    const showBtnContainer = document.getElementById("show-btn-container")
-    if(phones.length>5){
-       showBtnContainer.classList.remove('hidden')
-    }
-    else{
-        showBtnContainer.classList.add('hidden')
-    }
-    // slice to keep only 10 phone 
-    phones = phones.slice(0, 10)
-
-//   phone loop 
+  //show more btn condition
+  const showBtnContainer = document.getElementById("show-btn-container");
+  if (phones.length > 10 && !isShow) {
+    showBtnContainer.classList.remove("hidden");
+  } else {
+    showBtnContainer.classList.add("hidden");
+  }
+  // slice to keep only 10 phone
+  if (!isShow) {
+    phones = phones.slice(0, 10);
+  }
+  //   phone loop
   phones.forEach((phone) => {
     const phoneCard = document.createElement("div");
     phoneCard.innerHTML = `
@@ -35,35 +35,65 @@ const displayPhone = (phones) => {
   <div class="card-body items-center text-center">
     <h2 class="card-title mb-2">${phone.phone_name}</h2>
     <div class="card-actions">
-      <button class="btn btn-primary">Buy Now</button>
+      <button class="btn btn-primary" onclick="detailsHandler('${phone.slug}')">Details</button>
     </div>
   </div>
 </div>
         `
-
-        cardContainer.appendChild(phoneCard)
-       loading(false)
+    cardContainer.appendChild(phoneCard);
+    loading(false);
   });
+};
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//details button
+const detailsHandler = async (id) => {
+const response  = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+const data = await response.json();
+const phone = data.data
+// display Phone modal 
+details_modal.showModal()
+const div = document.getElementById("more_details")
+div.innerHTML =`
+<div class="flex justify-center mb-8"><img src="${phone.image}"></div>
+<div class="ml-6 ">
+<h3 class="font-bold text-2xl my-4">${phone.name}</h3>
+<p class="text-lg"><span class="font-semibold">Storage :</span> ${phone.mainFeatures?.storage || 'no storage data'}</p>
+<p class="text-lg my-2"><span class="font-semibold">Display Size :</span> ${phone.mainFeatures?.displaySize}</p>
+<p class="text-lg "><span class="font-semibold">ID :</span> ${phone.slug}</p>
+<p class="text-lg my-2"><span class="font-semibold">Release Date :</span> ${phone.releaseDate}</p>
+<p class="text-lg "><span class="font-semibold">GPS :</span> ${phone}</p>
+<p class="text-lg my-2"><span class="font-semibold">Memory :</span> ${phone.mainFeatures?.memory || 'data is not available'}</p>
+<p class="text-lg "><span class="font-semibold">Chipset :</span> ${phone.mainFeatures?.chipSet || 'data is not available'}</p>
+
+</div>
+`
+
+console.log(phone)
 };
 
 
-const searchBtn = () =>{
-    loading(true);
-    const searchFld = document.getElementById("search-field");
-    const searchText = searchFld.value ;
-    loadPhone(searchText);
-}
 
-const loading = (isLoading) =>{
-    const loadingContainer = document.getElementById('loading-container');
-    if(isLoading){
-       loadingContainer.classList.remove('hidden')
-   }
-   else{
-    loadingContainer.classList.add('hidden')
-   }
-}
 
-// const showBtn = (target) => {
 
-// }
+
+
+// search button
+const searchBtn = (isShow) => {
+  loading(true);
+  const searchFld = document.getElementById("search-field");
+  const searchText = searchFld.value;
+  loadPhone(searchText, isShow);
+};
+
+const loading = (isLoading) => {
+  const loadingContainer = document.getElementById("loading-container");
+  if (isLoading) {
+    loadingContainer.classList.remove("hidden");
+  } else {
+    loadingContainer.classList.add("hidden");
+  }
+};
+
+const showBtn = () => {
+  searchBtn(true);
+};
